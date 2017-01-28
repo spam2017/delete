8a7cfcf4af3d31e8732cd385891f9c117cb0041d
spam2017 <spam_2@yahoo.com>
2017-01-28 20:03:09 +0330

    Update and rename beyond.sh to delete.sh

--- delete.sh
+++ delete.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDReborn
+cd $HOME/delete
 
 install() {
 	    cd tg
@@ -31,13 +31,13 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
 fi
    echo -e "\033[38;5;208m"
-   echo -e "     > BeyondTeam Source :D                        "
+   echo -e "     > deleteTeam Source :D                        "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
 fi
 
-# Now All Argument Support after ./beyond.sh !
+# Now All Argument Support after ./delete.sh !
 #	Arguments :
 #			#			#			#			#			#			#			#			#			#
 #  --phone/-u                           specify username (would not be asked during authorization)
